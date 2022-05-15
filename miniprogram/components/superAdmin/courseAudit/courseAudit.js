5bde187b9370904f874aed8181f1744c696a3263
gzp <guozipeng@talkmoney.cn>
2022-05-15 12:37:42 +0800

    <U> 审核列表样式优化，组件封装

---
+++ miniprogram/components/superAdmin/courseAudit/courseAudit.js
@@ -0,0 +1,28 @@
+// components/superAdmin/courseAudit.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    courseInfo: Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    preview(){
+      console.log('预览');
+    },
+    edit(){
+      console.log('编辑');
+    }
+  }
+})
---
+++ miniprogram/components/superAdmin/visitorCard/visitorCard.js
@@ -0,0 +1,28 @@
+// components/superAdmin/visitorCard/visitorCard.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    visitorInfo: Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    click(){
+      console.log('click');
+    },
+    delete(){
+      console.log('delete');
+    }
+  }
+})
